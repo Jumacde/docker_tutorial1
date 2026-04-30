8fa03a39a44dadcf13ee299edd1843d02731fb7a
Yuma Takamura <y.takamura0407@gmail.com>
2026-04-30 21:48:53 +0200

    Commited

--- app/src/static/js/app.js
+++ app/src/static/js/app.js
@@ -55,7 +55,7 @@ function TodoListCard() {
             {items.length === 0 && (
                 //<p className="text-center">No items yet! Add one above!</p>
                 //<p className="text-center">You have no todo items yet! Add one above!</p>
-                <p className="text-center">nooo! Add one above!</p>
+                <p className="text-center">nooo!!! Add one above!</p>
             )}
             {items.map(item => (
                 <ItemDisplay
